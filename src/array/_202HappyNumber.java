bf2305bd09596998b77cd9a344fd20e04c509a14
xung-mi <xungmi1909@gmail.com>
2024-12-25 00:33:55 +0700

    202 array

--- src/array/_202HappyNumber.java
+++ src/array/_202HappyNumber.java
@@ -1,21 +1,24 @@
 package array;
 
 public class _202HappyNumber {
-	public boolean isHappy(int n) {
-		if (n == 1) {
-			return true;
-		}
-
-		if (n >= 2 && n <= 9){
-			return false;
-		}
-
+	public int getNext(int n){
 		int sum = 0;
 		while (n > 0){
 			int donvi = n%10;
 			sum += (int) Math.pow(donvi, 2);
 			n = n /10;
 		}
-		return isHappy(sum);
+		return sum;
+	}
+	public boolean isHappy(int n) {
+		int slow = n;
+		int fast = getNext(n);
+
+		while (fast != slow && fast != 1){
+			slow = getNext(slow);
+			fast = getNext(getNext(fast));
+		}
+
+		return fast == 1;
 	}
 }
