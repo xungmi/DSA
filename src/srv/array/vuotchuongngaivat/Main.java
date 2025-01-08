81414ef6727ce1850b2035d9442a3729a6873bfa
xung-mi <xungmi1909@gmail.com>
2025-01-08 23:44:02 +0700

    so dep

---
+++ src/srv/array/vuotchuongngaivat/Main.java
@@ -0,0 +1,40 @@
+//package srv.array.vuotchuongngaivat;
+//
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Scanner;
+//
+//public class Main {
+//	public static void main(String[] args) throws FileNotFoundException {
+//		System.setIn(new FileInputStream("D:\\code\\DSA\\src\\srv\\array\\vuotchuongngaivat\\input.txt"));
+//
+//		Scanner sc = new Scanner(System.in);
+//		int T, N, x, y;
+//		T = sc.nextInt();
+//		for(int tc = 0; tc < T; tc++){
+//			N = sc.nextInt();
+//			int[][] arr = new int[N][N];
+//			x = sc.nextInt();
+//			y = sc.nextInt();
+//			for (int i = 0; i < N; i++){
+//				for (int j = 0; j < N; j++){
+//					arr[i][j] = sc.nextInt();
+//				}
+//			}
+//			int count = 0;
+//			while(hasNext()){
+//				count++;
+//			}
+//
+//			System.out.println("#" + (tc+1) + " " + count);
+//		}
+//	}
+//	static boolean hasNext(int[][] arr, int x, int y){
+//		int[] save = new int[]{Integer.MAX_VALUE, -1, -1};
+//		if (x-1 >= 0 && y-1 >= 0 && arr[0] < ){
+//
+//		}
+//	}
+//}
